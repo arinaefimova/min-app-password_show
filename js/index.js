f9b6043c9fcbc123156aa644adc6932d24d933aa
kot0fey <arina.efimova162002@mail.ru>
2023-10-18 19:02:46 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+let eyeIcon =document.querySelector('#eyeicon');
+let password =document.querySelector('#password');
+password.onfocus=function(){
+    password.placeholder='';
+}
+password.onblur = function() {
+    if (password.value === '') {
+        password.placeholder = 'password';
+    }
+};
+
+eyeIcon.addEventListener('click',function(){
+    if(password.type == 'password'){
+        password.type ='text';
+        eyeIcon.src ='../img/eye-open.png';
+    }
+    else{
+        password.type = 'password';
+        eyeIcon.src ='../img/eye-close.png'
+    }
+})
+
